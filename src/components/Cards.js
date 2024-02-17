976dbf1f742a0ca031335948e98ec26cd3af346b
Anthony Doan-Ha <adoanha@ucla.edu>
2024-02-17 10:58:44 -0800

    Added more cards to the home page

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -10,15 +10,9 @@ function Cards() {
         <div className="cards__wrapper">
           <ul className="cards__items">
             <CardItem
-              src="images/humphery-img-1.jpg"
-              text="Humphrey basking in the sunlight, baking into a beautiful loaf"
-              label="Loaf"
-              path="/send-love"
-            />
-            <CardItem
-              src="images/humphery-img-2.jpg"
-              text={`"I'm gunna take a nap right :3"`}
-              label="Relaxed"
+              src="images/humphery-img-6.jpg"
+              text={`*sneeef* "You Stimnky"`}
+              label="Sneef"
               path="/send-love"
             />
           </ul>
@@ -39,6 +33,40 @@ function Cards() {
               src="images/humphery-img-5.jpg"
               text="Getting real personal, maybe a little too personal... 🫦"
               label="Sensual"
+              path="/send-love"
+            />
+          </ul>
+          <ul className="cards__items">
+            <CardItem
+              src="images/humphery-img-1.jpg"
+              text="Humphrey basking in the sunlight, baking into a beautiful loaf"
+              label="Loaf"
+              path="/send-love"
+            />
+            <CardItem
+              src="images/humphery-img-2.jpg"
+              text={`"I'm gunna take a nap right here"`}
+              label="Relaxed"
+              path="/send-love"
+            />
+          </ul>
+          <ul className="cards__items">
+            <CardItem
+              src="images/humphery-img-8.jpg"
+              text="What are you staring at?"
+              label="Stare"
+              path="/send-love"
+            />
+            <CardItem
+              src="images/humphery-img-9.jpg"
+              text="Hiding underneath Jekka's bed sksksk >:3"
+              label="Hiding"
+              path="/send-love"
+            />
+            <CardItem
+              src="images/humphery-img-10.jpg"
+              text="Chilling with my bestie"
+              label="Chill"
               path="/send-love"
             />
           </ul>
